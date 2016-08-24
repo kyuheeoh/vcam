5ca9b50f191bf894429f7a9529dc880b05ff2dbd
Dr.Q <kyuheeoh@hotmail.com>
2016-08-24 13:16:35 +0900

    revised license and coding scheme

--- framebuffertest.cpp
+++ framebuffertest.cpp
@@ -1,7 +1,7 @@
-/*
+/*    
  * framebuffertest.cpp
  * 
- * Copyright 2016 DrK <pepper@DrK-Desktop>
+ * Copyright 2016 Dr.Q <kyuheeoh@hotmail.com>
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,15 +22,15 @@
  */
 
 /*
-To test that the Linux framebuffer is set up correctly, and that the device permissions
-are correct, use the program below which opens the frame buffer and draws a gradient-
-filled red square:
+ *To test that the Linux framebuffer is set up correctly, and that the device permissions
+ *are correct, use the program below which opens the frame buffer and draws a gradient-
+ *filled red square:
 
-retrieved from:
-Testing the Linux Framebuffer for Qtopia Core (qt4-x11-4.2.2)
+ *retrieved from:
+ *Testing the Linux Framebuffer for Qtopia Core (qt4-x11-4.2.2)
 
-http://cep.xor.aps.anl.gov/software/qt4-x11-4.2.2/qtopiacore-testingframebuffer.html
-*/
+ *http://cep.xor.aps.anl.gov/software/qt4-x11-4.2.2/qtopiacore-t estingframebuffer.html
+ */
 
 #include <stdlib.h>
 #include <unistd.h>
@@ -54,20 +54,23 @@ int main()
 
     // Open the file for reading and writing
     fbfd = open("/dev/fb0", O_RDWR);
-    if (fbfd == -1) {
+    if (fbfd == -1)
+    {
         perror("Error: cannot open framebuffer device");
         exit(1);
     }
     printf("The framebuffer device was opened successfully.\n");
 
     // Get fixed screen information
-    if (ioctl(fbfd, FBIOGET_FSCREENINFO, &finfo) == -1) {
+    if (ioctl(fbfd, FBIOGET_FSCREENINFO, &finfo) == -1) 
+    {
         perror("Error reading fixed information");
         exit(2);
     }
 
     // Get variable screen information
-    if (ioctl(fbfd, FBIOGET_VSCREENINFO, &vinfo) == -1) {
+    if (ioctl(fbfd, FBIOGET_VSCREENINFO, &vinfo) == -1) 
+    {
         perror("Error reading variable information");
         exit(3);
     }
@@ -79,33 +82,31 @@ int main()
 
     // Map the device to memory
     fbp = (char *)mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fbfd, 0);
-    if (!fbp) {
+    
+    if (!fbp) 
+    {
         perror("Error: failed to map framebuffer device to memory");
         exit(4);
     }
+    
     printf("The framebuffer device was mapped to memory successfully.\n");
 
     x = 100; y = 100;       // Where we are going to put the pixel
 
     // Figure out where in memory to put the pixel
     for (y = 100; y < 300; y++)
-        for (x = 100; x < 300; x++) {
-
-            location = (x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
+        for (x = 100; x < 300; x++)
+        {
+			location = (x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
                        (y+vinfo.yoffset) * finfo.line_length;
 
-            if (vinfo.bits_per_pixel == 32) {
-                *(fbp + location) = 100;        // Some blue
-                *(fbp + location + 1) = 15+(x-100)/2;     // A little green
-                *(fbp + location + 2) = 200-(y-100)/5;    // A lot of red
-                *(fbp + location + 3) = 0;      // No transparency
-        //location += 4;
-            } else  { //assume 16bpp
-                int b = 10;
-                int g = (x-100)/6;     // A little green
-                int r = 31-(y-100)/16;    // A lot of red
-                unsigned short int t = r<<11 | g << 5 | b;
-                *((unsigned short int*)(fbp + location)) = t;
+            if (vinfo.bits_per_pixel == 32)
+            {
+                *(fbp + location) = 100;        		// B
+                *(fbp + location + 1) = 15+(x-100)/2;   // G
+                *(fbp + location + 2) = 200-(y-100)/5;  // R
+                *(fbp + location + 3) = 0;      		// No transparency
+														//location += 4;
             }
 
         }
